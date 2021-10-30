a84faa4849dc870410fb73c792d233139e8295f4
Bety Martinez <trism460@gmail.com>
2021-10-29 20:12:27 -0500

    Add image & text

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+// import App from './App';
+import HolaMundo from './holaMundo';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => HolaMundo);
