9757fa63dd0c7f13323072ba9e611e6fd331685a
Bety Martinez <trism460@gmail.com>
2021-10-30 18:36:26 -0500

    Style with alignItems

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import JustifyContentBasics from './src/components/justifyContent';
+import AlignItemsLayout from './src/components/alignItems';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => JustifyContentBasics);
+AppRegistry.registerComponent(appName, () => AlignItemsLayout);
