5697b3e9d12062ec11dbd81fe77b3ce405a98f3d
Bety Martinez <trism460@gmail.com>
2021-10-30 10:37:35 -0500

    Add styles with StyleSheet.create

--- index.js
+++ index.js
@@ -4,7 +4,8 @@
 
 import {AppRegistry} from 'react-native';
 // import App from './App';
-import HolaMundo from './holaMundo';
+// import HolaMundo from './holaMundo';
+import LotsOfStyles from './src/components/style';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => HolaMundo);
+AppRegistry.registerComponent(appName, () => LotsOfStyles);
