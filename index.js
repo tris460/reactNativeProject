d0f73b495873c47122d3c02b3db51f4d90ae5242
Bety Martinez <trism460@gmail.com>
2021-10-30 18:26:06 -0500

    Style with flexDirection

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import Flex from './src/components/flex';
+import FlexDirectionBasics from './src/components/flexDirection';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => Flex);
+AppRegistry.registerComponent(appName, () => FlexDirectionBasics);
