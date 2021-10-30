ca3351d944c4a2f52bd57add44ed124067aa8821
Bety Martinez <trism460@gmail.com>
2021-10-30 18:43:15 -0500

    Style with alignSelf

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import AlignItemsLayout from './src/components/alignItems';
+import AlignSelfLayout from './src/components/alignSelf'
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => AlignItemsLayout);
+AppRegistry.registerComponent(appName, () => AlignSelfLayout);
