613cd022a8a7f1f580c02966a5cf57fbf303de78
Bety Martinez <trism460@gmail.com>
2021-10-30 19:01:08 -0500

    Style with width & height

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './src/components/flexBGS';
+import WidthHeightBasics from './src/components/widthHeight';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => WidthHeightBasics);
