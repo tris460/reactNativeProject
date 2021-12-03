44085b14d5c546308ca719132c4305916d24e2bc
Bety Martinez <trism460@gmail.com>
2021-12-03 01:10:45 -0600

    Add ScrollView in editProducts

--- src/components/editProduct.js
+++ src/components/editProduct.js
@@ -1,43 +1,45 @@
 import React from "react";
-import { StyleSheet, View, Image, TextInput, TouchableOpacity, Text } from "react-native";
+import { StyleSheet, View, Image, TextInput, TouchableOpacity, Text, ScrollView } from "react-native";
 
 const editProduct = ({ navigation }) => {
     return (
-        <View style={styles.container}>
-            <Image 
-            style={styles.logo} 
-            source={require('../../assets/logo.jpeg')}></Image>
-            <TextInput
-                style={styles.input}
-                placeholder="Product name"
-            />
-            <TextInput
-                style={styles.input}
-                placeholder="Price"
-                keyboardType="numeric"
-            />
-            <TextInput
-                style={styles.input}
-                placeholder="Image"
-            />
-            <TextInput
-                style={styles.input}
-                placeholder="Condition"
-            />
-            <TextInput
-                style={styles.input}
-                placeholder="Category"
-            />
-            <TextInput
-                style={styles.input}
-                placeholder="Description"
-            />
-            <TouchableOpacity
-                style={styles.button}
-            >
-                <Text style={styles.textButton}>Add Product</Text>    
-            </TouchableOpacity>            
-        </View>
+        <ScrollView>
+            <View style={styles.container}>
+                <Image 
+                style={styles.logo} 
+                source={require('../../assets/logo.jpeg')}></Image>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Product name"
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder="Price"
+                    keyboardType="numeric"
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder="Image"
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder="Condition"
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder="Category"
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder="Description"
+                />
+                <TouchableOpacity
+                    style={styles.button}
+                >
+                    <Text style={styles.textButton}>Add Product</Text>    
+                </TouchableOpacity>            
+            </View>
+        </ScrollView>
     );
 };
 
