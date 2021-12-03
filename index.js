20df9852773d0084dcefd5701aa9b2074fb80a67
Bety Martinez <trism460@gmail.com>
2021-12-02 23:51:22 -0600

    Initial design for products

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ import {AppRegistry} from 'react-native';
 // import login from './src/components/login';
 // import menu from './src/components/menu';
 // import editProduct from './src/components/editProduct';
-// import products from './src/components/products';
-import about from './src/components/about'
+import products from './src/components/products';
+// import about from './src/components/about'
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => about);
+AppRegistry.registerComponent(appName, () => products);
---
+++ src/components/products.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { StyleSheet, View, Image, Text, ScrollView } from "react-native";
+
+const products = () => {
+    return (
+        <ScrollView>
+            <View style={styles.container}>
+                <Image 
+                style={styles.logo} 
+                source={require('../../assets/logo.jpeg')}></Image>
+                <View style={styles.container}>
+                    <View style={styles.productContainer}>
+                        <View style={styles.imgProduct}>
+                            <Image style={styles.image} source={{uri: 'https://tse3.mm.bing.net/th?id=OIP.a46KCUdEuRQPp-DIrUegTAHaFj&pid=Api'}}></Image>
+                        </View>
+                        <View style={styles.textProduct}>
+                            <Text style={styles.text, styles.textName}>Product Name</Text>
+                            <Text style={styles.text, styles.price}>Price</Text>
+                            <Text style={styles.text, styles.description}>Description</Text>
+                        </View>
+                    </View>
+                </View>
+            </View>
+        </ScrollView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        height: "100%",
+        width: "100%",
+        flex: 1,
+    },
+    productContainer: {
+        flexWrap: "wrap",
+        maxHeight: 100,
+    },
+    logo: {
+        width: 130,
+        height: 130,
+        alignSelf: 'center',
+        marginTop: 15,
+    },
+    imgProduct: {
+        width: 130,
+        height: 100,
+        marginLeft: 5
+    },
+    image: {
+        height: '70%',
+        width: '70%'
+    }, 
+    textProduct: {},
+    textName: {
+        fontSize: 18,
+        color: "#333",
+    },
+    price: {
+        fontSize: 15,
+        color: "#111",
+    },
+    description: {},
+})
+
+export default products;
