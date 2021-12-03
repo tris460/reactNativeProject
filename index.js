fd6d2b7c18e7a7b86b28d6e5d8101908a3b937e8
Bety Martinez <trism460@gmail.com>
2021-12-02 23:33:31 -0600

    Initial design for about

--- index.js
+++ index.js
@@ -5,7 +5,9 @@
 import {AppRegistry} from 'react-native';
 // import login from './src/components/login';
 // import menu from './src/components/menu';
-import editProduct from './src/components/editProduct';
+// import editProduct from './src/components/editProduct';
+// import products from './src/components/products';
+import about from './src/components/about'
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => editProduct);
+AppRegistry.registerComponent(appName, () => about);
---
+++ src/components/about.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { StyleSheet, View, Image, Text } from "react-native";
+
+const about = () => {
+    return (
+        <View style={styles.container}>
+            <Image 
+            style={styles.logo} 
+            source={require('../../assets/logo.jpeg')}></Image>
+            <Text style={styles.textName}>Ana María Pérez Escalante</Text>
+            <Text style={styles.textID}>190839</Text>
+            <Text style={styles.textName}>Beatriz Martínez Pérez</Text>
+            <Text style={styles.textID}>190343</Text>
+            <Text style={styles.textName}>Paola Cristina Rivas Esparza</Text>
+            <Text style={styles.textID}>190463</Text>
+            <Text style={styles.textName}>Christian Emmanuel Davis Castañeda</Text>
+            <Text style={styles.textID}>200131</Text>
+            <Text style={styles.group}>4°B</Text>
+            <Text style={styles.subject}>DAPPS</Text>
+            <Image 
+            style={styles.uni}
+            source={{uri: 'https://ariaac.mx/wordpress/wp-content/uploads/2018/08/Universidad-Tecnol%C3%B3gica-de-Aguascalientes.jpg'}}></Image>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        height: "100%",
+        width: "100%",
+        flex: 1,
+    },
+    logo: {
+        width: 150,
+        height: 150,
+        alignSelf: 'center',
+        marginTop: 20,
+    },
+    textName: {
+        fontSize: 20,
+        marginTop: 10,
+        marginLeft: 10,
+        color: '#444'
+    },
+    textID: {
+        marginLeft: 15,
+    },
+    group: {
+        textAlign: 'center',
+        marginTop: 30,
+        color: '#555'
+    },
+    subject: {
+        textAlign: 'center',
+        color: '#555',
+    },
+    uni: {
+        height: 100,
+        width: 100,
+        alignSelf: 'center',
+
+    },
+})
+
+export default about;
