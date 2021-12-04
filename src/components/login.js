cb7e64eacd9fe56d053cd05511f36b313c509a03
Bety Martinez <trism460@gmail.com>
2021-12-03 20:52:03 -0600

    Add ScrollView/Hide password

--- src/components/login.js
+++ src/components/login.js
@@ -1,30 +1,33 @@
 import React from "react";
-import { StyleSheet, View, Image, TextInput, TouchableOpacity, Text } from "react-native";
+import { StyleSheet, View, Image, TextInput, TouchableOpacity, Text, ScrollView } from "react-native";
 
 const login = ({ navigation }) => {
     return (
-        <View style={styles.container}>
-            <Image 
-            style={styles.logo} 
-            source={require('../../assets/logo.jpeg')}></Image>
-            <TextInput
-                style={styles.input}
-                placeholder="E-mail"
-            />
-            <TextInput
-                style={styles.input}
-                placeholder="Password"
-                password={true}
-            />
-            <TouchableOpacity
-                style={styles.button}
-                onPress = {() => {
-                    navigation.navigate('Menu')
-                }}
-            >
-                <Text style={styles.textButton}>Login</Text>    
-            </TouchableOpacity>             
-        </View>
+        <ScrollView>
+            <View style={styles.container}>
+                <Image 
+                style={styles.logo} 
+                source={require('../../assets/logo.jpeg')}></Image>
+                <TextInput
+                    style={styles.input}
+                    placeholder="E-mail"
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder="Password"
+                    password={true}
+                    secureTextEntry={true}
+                />
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress = {() => {
+                        navigation.navigate('Menu')
+                    }}
+                >
+                    <Text style={styles.textButton}>Login</Text>    
+                </TouchableOpacity>             
+            </View>
+        </ScrollView>
     );
 };
 
@@ -33,6 +36,7 @@ const styles = StyleSheet.create({
         height: "100%",
         width: "100%",
         display: 'flex',
+        backgroundColor: "#fff",
     },
     logo: {
         width: 250,
