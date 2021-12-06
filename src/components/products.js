76700d40beb021cf4226c0377e2758c033c737a0
Bety Martinez <trism460@gmail.com>
2021-12-06 15:57:43 -0600

    Update styles for products

--- src/components/products.js
+++ src/components/products.js
@@ -9,6 +9,223 @@ import {
 } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
+// const products = ({ navigation }) => {
+//     const [response, setResponse] = useState(0);
+    
+//     const getData = () => {
+//         axios
+//             .get('https://www.fruityvice.com/api/fruit/all')
+//             .then((res) => {
+//                 let r = res.data.map((item) => {
+//                     return [item];
+//                 });
+//                 setResponse(r);
+//             })
+//             .catch((error) => {
+//                 console.log(error);
+//             });
+//     };
+//     return (
+//         <ScrollView>
+//                     <View style={styles.container}>
+//                         <Image 
+//                         style={styles.logo} 
+//                         source={require('../../assets/logo.jpeg')}></Image>
+//                         <View style={styles.container}>
+//                             <View style={styles.productContainer}>
+//                                 <View style={styles.imgProduct}>
+//                                     <Image style={styles.image} source={{uri: 'https://tse3.mm.bing.net/th?id=OIP.a46KCUdEuRQPp-DIrUegTAHaFj&pid=Api'}}></Image>
+//                                 </View>
+//                                 <View style={styles.textProduct}>
+//                                     <Text style={styles.text, styles.textName}>{response}</Text>
+//                                 </View>
+//                             </View>
+//                         </View>
+//                         </View>
+//                         <TouchableOpacity
+//                             style={styles.button}
+//                             onPress = {getData()}
+//                         >
+//                             <Text style={styles.textButton}>Get Data</Text>    
+//                         </TouchableOpacity>
+//                 </ScrollView>          
+//     )
+// }
+
+// const styles = StyleSheet.create({
+//     container: {
+//         height: "100%",
+//         width: "100%",
+//         flex: 1,
+//         backgroundColor: "#fff",
+//     },
+//     productContainer: {
+//         margin: 5,
+//         padding: 3,
+//         flexWrap: "wrap",
+//         maxHeight: 100,
+//         borderColor: "#CAE2FE",
+//         borderRadius: 5,
+//         borderWidth: 2
+//     },
+//     logo: {
+//         width: 130,
+//         height: 130,
+//         alignSelf: 'center',
+//         marginTop: 15,
+//     },
+//     imgProduct: {
+//         width: 130,
+//         height: 100,
+//         marginLeft: 5
+//     },
+//     image: {
+//         height: '70%',
+//         width: '70%'
+//     }, 
+//     textProduct: {},
+//     textName: {
+//         fontSize: 18,
+//         color: "#333",
+//     },
+//     price: {
+//         fontSize: 15,
+//         color: "#111",
+//     },
+//     description: {},
+//     button: {
+//         backgroundColor: "#CAE2FE",
+//         margin: 10,
+//         padding: 15,
+//         borderRadius: 10,
+//         alignItems: "center"
+//     },
+//     textButton: {
+//         fontSize: 18,
+//         color: "#fff"
+//     },
+// })
+
+// export default products;
+
+// export default class products extends React.Component {
+//     constructor(props) { 
+//         super(props);
+
+//         this.state = {
+//             loading: false,
+//             fruits: '',
+//             url: 'https://www.fruityvice.com/api/fruit/all',
+//         };
+//     }
+
+//     componentDidMount() {
+//         this.getData();
+//     }
+
+//     getData = () => {
+//         this.setState({ loading: true});
+
+//         fetch(this.state.url)
+//         .then((res) => res.json())
+//         .then((res) => {
+//             console.log(res)
+//         })
+//         .then((res) => {
+//             this.setState({
+//                 loading: false,
+//                 fruits: res,
+//             });
+//         })
+//         .then(console.log('----------------------', this.state.fruits))
+//         .then(console.log(typeof this.state))
+//         .catch((err) => {
+//             console.error(err)
+//         })
+//     }
+
+//     render() {
+//         return (
+//            <View>
+//                 <View style={styles.container}>
+//                     <View style={styles.productContainer}>
+//                         <FlatList
+//                             data={this.state.fruits}
+//                             renderItem={
+//                                 ({item}) => 
+//                                 <View style={styles.textProduct}>
+//                                     <Text style={styles.text, styles.textName}>Family: {item.family}</Text>
+//                                     <Text style={styles.text, styles.textName}>Genus: {item.genus}</Text>
+//                                     <Text style={styles.text, styles.textName}>Name: {item.name}</Text>
+//                                     <Text style={styles.text, styles.textName}>Nutritions: {item.nutritions.calories}</Text>
+//                                     <Text style={styles.text, styles.textName}>Nutritions: {item.nutritions.carbohydrates}</Text>
+//                                     <Text style={styles.text, styles.textName}>Nutritions: {item.nutritions.fat}</Text>
+//                                     <Text style={styles.text, styles.textName}>Nutritions: {item.nutritions.protein}</Text>
+//                                     <Text style={styles.text, styles.textName}>Nutritions: {item.nutritions.sugar}</Text>
+//                                 </View>
+//                             }
+//                         />
+//                     </View>
+//                 </View>
+//             </View>
+//         )
+//     }
+// }
+
+// const styles = StyleSheet.create({
+//     container: {
+//         height: "100%",
+//         width: "100%",
+//         flex: 1,
+//         backgroundColor: "#fff",
+//     },
+//     productContainer: {
+//         margin: 5,
+//         padding: 3,
+//         flexWrap: "wrap",
+//         maxHeight: 100,
+//         borderColor: "#1e6d65",
+//         borderRadius: 5,
+//         borderWidth: 2
+//     },
+//     logo: {
+//         width: 130,
+//         height: 130,
+//         alignSelf: 'center',
+//         marginTop: 15,
+//     },
+//     imgProduct: {
+//         width: 130,
+//         height: 100,
+//         marginLeft: 5
+//     },
+//     image: {
+//         height: '70%',
+//         width: '70%'
+//     }, 
+//     textProduct: {},
+//     textName: {
+//         fontSize: 18,
+//         color: "#333",
+//     },
+//     price: {
+//         fontSize: 15,
+//         color: "#111",
+//     },
+//     description: {},
+//     button: {
+//         backgroundColor: "#1e6d65",
+//         margin: 10,
+//         padding: 15,
+//         borderRadius: 10,
+//         alignItems: "center"
+//     },
+//     textButton: {
+//         fontSize: 18,
+//         color: "#fff"
+//     },
+// })
+
 export default function App() {
     const [fruits, setFruits] = useState([]);
     const [expandedIndex, setExpandedIndex] = useState(-1);
@@ -30,16 +247,28 @@ export default function App() {
           : setExpandedIndex(index)
         }>
           <View style={styles.fruitContainer}>
-            <Text style={[styles.fruitName, styles.regularText]}>
+            <Text style={styles.fruitName}>
               Name: {fruitItem.name}
             </Text>
             <Text style={styles.regularText}>Family: {fruitItem.family}</Text>
             {expandedIndex === index && (
-              <Text style={styles.regularText}>
-                Carbohydrates: {carbohydrates}g, Protein:{' '}
-                {protein}g, Calories: {calories}g, Sugar:{' '}
-                {sugar}g, Fat: {fat}g
-              </Text>
+              <View>
+                <Text style={styles.regularText}>
+                  Carbohydrates: {carbohydrates}g
+                </Text>
+                <Text style={styles.regularText}>
+                  Protein:{' '} {protein}g, 
+                </Text>
+                <Text style={styles.regularText}>
+                  Calories: {calories}g
+                </Text>
+                <Text style={styles.regularText}>
+                  Sugar:{' '}{sugar}g
+                </Text>
+                <Text style={styles.regularText}>
+                  Fat: {fat}g
+                </Text>
+              </View>
             )}
           </View>
         </Pressable>
@@ -72,16 +301,18 @@ export default function App() {
     },
     fruitContainer: {
       margin: 5,
-      shadowRadius: 20,
-      shadowColor: '#fff',
-      shadowOffset: {width: 10, height: 10},
+      padding: 8,
+      backgroundColor: "#1e6d65",
+      borderRadius: 5,
     },
     fruitName: {
-      color: '#111111',
-      fontSize: 16,
+      fontSize: 18,
+      color: '#fff',
+      fontWeight: '600',
+      marginLeft: 5,
     },
     regularText: {
-      color: '#000',
-      fontWeight: '600',
+      color: '#eee',
+      fontSize: 14,
     },
   });
